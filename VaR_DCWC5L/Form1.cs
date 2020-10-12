cab6c9a2bb37307e2c78efc8411166688b9eaf94
Marianna Barta <marianna.barta@stud.uni-corvinus.hu>
2020-10-12 19:05:49 +0200

    commit-feladat9

--- VaR_DCWC5L/Form1.cs
+++ VaR_DCWC5L/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,9 +17,13 @@ namespace VaR_DCWC5L
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
         List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        List<string> Output;
 
         public Form1()
         {
+            string[,] array = new string[1000, 2];
+            Output = new List<string>();
+            
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
@@ -35,6 +40,7 @@ namespace VaR_DCWC5L
                            - GetPortfolioValue(kezdőDátum.AddDays(i));
                 Nyereségek.Add(ny);
                 Console.WriteLine(i + " " + ny);
+                Output.Add(ny.ToString());
             }
 
             var nyereségekRendezve = (from x in Nyereségek
@@ -65,6 +71,31 @@ namespace VaR_DCWC5L
                 value += (decimal)last.Price * item.Volume;
             }
             return value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "Text File | *.txt";
+            sfd.DefaultExt = "txt";
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                string output = "";
+                sw.WriteLine(String.Format("{0}\t{1}", "Időszak", "Nyereség"));
+
+                sw.WriteLine(output);
+                for (int i = 0; i < Output.Count(); i++)
+                {
+
+                    output += String.Format("{0}\t{1}", i.ToString(), Output[i].ToString());
+
+                    sw.WriteLine(output);
+                    output = "";
+                }
+            }
         }
     }
 }
