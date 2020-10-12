386de52effc8d4e959af07db037c400f08bb1839
Marianna Barta <marianna.barta@stud.uni-corvinus.hu>
2020-10-12 18:24:27 +0200

    commit-feladat4

--- VaR_DCWC5L/Form1.cs
+++ VaR_DCWC5L/Form1.cs
@@ -12,9 +12,15 @@ namespace VaR_DCWC5L
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+            List < Tick > 1 = context.Ticks.ToList();
         }
     }
 }
