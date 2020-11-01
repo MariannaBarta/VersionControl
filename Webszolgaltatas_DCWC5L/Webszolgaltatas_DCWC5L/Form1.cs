1353c0b67e1f34e658fab44017bfe1c0df573218
Marianna Barta <marianna.barta@stud.uni-corvinus.hu>
2020-11-01 18:16:32 +0100

    commit-feladat3

--- Webszolgaltatas_DCWC5L/Webszolgaltatas_DCWC5L/Form1.cs
+++ Webszolgaltatas_DCWC5L/Webszolgaltatas_DCWC5L/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Webszolgaltatas_DCWC5L.MnbServiceReference;
 
 namespace Webszolgaltatas_DCWC5L
 {
@@ -15,6 +16,28 @@ namespace Webszolgaltatas_DCWC5L
         public Form1()
         {
             InitializeComponent();
+
+            GetExchangeRates();
+
         }
+
+        private void GetExchangeRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
+        }
+
+       
     }
 }
