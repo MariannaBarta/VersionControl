1936c4d93d4042df0f9a921eefbb0935a95b640c
Marianna Barta <marianna.barta@stud.uni-corvinus.hu>
2020-11-22 18:48:46 +0100

    commit-feladat7

--- Szoftverteszteles_DCWC5L/Szoftverteszteles_DCWC5L/Form1.cs
+++ Szoftverteszteles_DCWC5L/Szoftverteszteles_DCWC5L/Form1.cs
@@ -41,7 +41,7 @@ namespace Szoftverteszteles_DCWC5L
             var maxPosition = 0;
             foreach (var ball in _balls)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition) maxPosition = ball.Left;
             }
 
