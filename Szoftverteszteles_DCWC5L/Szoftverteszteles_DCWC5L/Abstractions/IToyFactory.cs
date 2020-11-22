a1bd6907d76c63efc6c39917c3ad308c486dba1a
Marianna Barta <marianna.barta@stud.uni-corvinus.hu>
2020-11-22 18:51:50 +0100

    commit-feladat8

---
+++ Szoftverteszteles_DCWC5L/Szoftverteszteles_DCWC5L/Abstractions/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szoftverteszteles_DCWC5L.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
