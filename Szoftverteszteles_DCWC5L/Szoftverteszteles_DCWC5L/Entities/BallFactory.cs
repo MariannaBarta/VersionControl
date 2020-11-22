6bd3e7a6a3da9565f55ec20e98bb377ff7436cac
Marianna Barta <marianna.barta@stud.uni-corvinus.hu>
2020-11-22 18:35:19 +0100

    commit-feladat4

--- Szoftverteszteles_DCWC5L/Szoftverteszteles_DCWC5L/Entities/BallFactory.cs
+++ Szoftverteszteles_DCWC5L/Szoftverteszteles_DCWC5L/Entities/BallFactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Szoftverteszteles_DCWC5L.Entities
 {
-    class BallFactory
+    public class BallFactory
     {
         public Ball CreateNew()
         {
--- Szoftverteszteles_DCWC5L/Szoftverteszteles_DCWC5L/Form1.cs
+++ Szoftverteszteles_DCWC5L/Szoftverteszteles_DCWC5L/Form1.cs
@@ -7,14 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Szoftverteszteles_DCWC5L.Entities;
 
 namespace Szoftverteszteles_DCWC5L
 {
     public partial class Form1 : Form
     {
+        private List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            mainPanel.Controls.Add(ball);
+            ball.Left = -ball.Width;
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+                if (ball.Left > maxPosition) maxPosition = ball.Left;
+            }
+
+            if (maxPosition > 1000)
+            {
+                var oldestBall = _balls[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
         }
     }
 }
